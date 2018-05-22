f0ef3dcab64c0032c98fad4467172fc13c9b67e7
Manthan Thakar <manthant15@gmail.com>
2018-05-22 15:43:20 +0530

    Add capability to limit characters

--- wordcounter/static/js/counter.js
+++ wordcounter/static/js/counter.js
@@ -6,4 +6,13 @@ $(document).ready(function () {
             $("#count").val(result);
         })
     })
+
+    $("#text").keyup(function() {
+        var data = $('#text').val().trim();
+        $('#charcount').html(data.length + " / 50");
+        if (data.length > 50) {
+            $('#text').val(data.substr(0, 50));
+            // alert("You have exceeded the character limit of 50");
+        }
+    })
 })
